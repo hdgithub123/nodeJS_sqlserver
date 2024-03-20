20f6f378d5eb6132f067ba532f02d461cd32317a
hdgithub123 <haidanglink0@gmail.com>
2024-03-20 15:47:57 +0700

    change user control and model

--- App/controllers/user.controllers.js
+++ App/controllers/user.controllers.js
@@ -1,11 +1,10 @@
 // user.controller.js
 const userModel = require('../models/user.model');
 
-
-
-useslist = (req, res) => {
-    const userProfile = userModel.getUsersProfile();
+useslist = async(req, res) => { // bắt buộc dùng async... await
+    const userProfile = await userModel.getUsersProfile();
     res.send(userProfile);
+    console.log(userProfile.Status);
 };
 
 
--- App/models/user.model.js
+++ App/models/user.model.js
@@ -1,24 +1,13 @@
-const { executeQuery } = require("../../SQLServer/SqlServerConnect")
 
-// user.model.js
-getUsersProfile = () => {
-    // Logic để lấy thông tin người dùng từ cơ sở dữ liệu
-    Sqlstring = "Select * from users"
-    
-    // lấy dữ liệu từ DB
-    executeQuery(Sqlstring)
-    .then(response => {
-        // Truy cập vào kết quả và thông báo từ Promise
-            console.log(response);
-            return(JSON.stringify(response))
+const sqldata = require("../../SQLServer/SqlServerConnect");
 
-    })
-    .catch(error => {
-        // Xử lý lỗi từ Promise
-        console.error("Promise Error:", error);
-    });
+ function getUsersProfile() {
+        // Logic để lấy thông tin người dùng từ cơ sở dữ liệu
+        const Sqlstring = "Select * from users";
+        const data =  sqldata.executeQuery(Sqlstring);
+        return data;
 
-};
+}
 
 module.exports = {
     getUsersProfile,
